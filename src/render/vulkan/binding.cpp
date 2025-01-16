77f37907e9ee3f905ac714aabc243482530d61ca
magistermaks <magistermaks@darktree.net>
2025-01-16 07:57:30 +0100

    Vertex descriptors

---
+++ src/render/vulkan/binding.cpp
@@ -0,0 +1,48 @@
+
+#include "binding.hpp"
+
+/*
+ * BindingLayout
+ */
+
+BindingLayout::BindingLayout(uint32_t stride, std::vector<VkVertexInputAttributeDescription> attributes, VkVertexInputRate rate)
+: stride(stride), attributes(attributes), vk_rate(rate) {}
+
+std::vector<VkVertexInputAttributeDescription> BindingLayout::getAttributes() const {
+	return attributes;
+}
+
+uint32_t BindingLayout::getStride() const {
+	return stride;
+}
+
+VkVertexInputRate BindingLayout::getRate() const {
+	return vk_rate;
+}
+
+/*
+ * BindingLayoutBuilder
+ */
+
+BindingLayoutBuilder::BindingLayoutBuilder(VkVertexInputRate rate)
+: offset(0), attributes(), vk_rate(rate) {}
+
+BindingLayoutBuilder BindingLayoutBuilder::begin(VkVertexInputRate rate) {
+	return {rate};
+}
+
+BindingLayoutBuilder& BindingLayoutBuilder::attribute(uint32_t location, VkFormat format) {
+
+	VkVertexInputAttributeDescription description {};
+	description.location = location;
+	description.format = format;
+	description.offset = offset;
+
+	offset += getFormatInfo(format).size;
+	attributes.push_back(description);
+	return *this;
+}
+
+BindingLayout BindingLayoutBuilder::done() {
+	return {offset, attributes, vk_rate};
+}
