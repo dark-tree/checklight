26a6ecb1bdfe1eea10f771154e447d5bb280f4a2
1Tymon1 <tymonbielski@gmail.com>
2025-03-01 22:47:19 +0100

    1st iteration of GJK

--- src/physics/PhysicsEngine.hpp
+++ src/physics/PhysicsEngine.hpp
@@ -96,6 +96,10 @@ public:
 
     bool manageSimplex(std::vector<glm::vec3>& simplex, glm::vec3& direction)
     {
+        if (simplex.size() > 4)
+        {
+            throw std::runtime_error {"Simplex has more than 4 vertexes!"};
+        }
         //in each of those functions the points on the simplex are lettered from newest to oldest (a-most recently added, d-least recently added)
         if (simplex.size() == 2)
         {
@@ -146,9 +150,7 @@ public:
             direction = tripleCrossProduct(ab, ao);
 
             //update the triangle points, we're not yet ready to form a tetrahedron
-            simplex[0] = simplex[1];
-            simplex[1] = simplex[2];
-            simplex.erase(simplex.begin() + 2);
+            simplex.erase(simplex.begin());
             //while we technically could go to a tetrahedron instead of a triangle here, it'll make our life easier and the algorithm faster in most cases
         } //check whether the origin is located in the AC Voronoi region (or above/below it)
         else if (glm::dot(voronoi_ac, ao) > 0)
@@ -156,10 +158,9 @@ public:
             //same case as above
             direction = tripleCrossProduct(ac, ao);
 
-            simplex[1] = simplex[2];
-            simplex.erase(simplex.begin() + 2);
+            simplex.erase(simplex.begin() + 1);
         } //if we got here that means the origin is within the triangle, we just need to check whether above or below
-        else if (glm::dot(normal, ao) > 0)
+        else if (glm::dot(normal, ao) >= 0)
         {
             //case for above
             //all that needs to be done is changing the search direction to the triangle's normal
@@ -178,7 +179,194 @@ public:
 
     bool tetrahedronCase(std::vector<glm::vec3>& simplex, glm::vec3& direction)
     {
-        //TODO
+        //as in the cases above we start with defining vectors
+        glm::vec3 ao = glm::vec3(0, 0, 0) - simplex[3];
+
+        glm::vec3 ab = simplex[2] - simplex[3];
+        glm::vec3 ac = simplex[1] - simplex[3];
+        glm::vec3 ad = simplex[0] - simplex[3];
+
+        glm::vec3 normal_abc = glm::cross(ab, ac);
+        glm::vec3 normal_acd = glm::cross(ac, ad);
+        glm::vec3 normal_adb = glm::cross(ad, ab);
+
+        //check if the origin is outside each given face
+        bool over_abc = glm::dot(normal_abc, ao) > 0;
+        bool over_acd = glm::dot(normal_acd, ao) > 0;
+        bool over_adb = glm::dot(normal_adb, ao) > 0;
+
+        if (!over_abc && !over_acd && !over_adb)
+        {
+            //origin within all faces of the tetrahedron - we have a collision!
+            return true;
+        }
+        else if (over_abc && !over_acd && !over_adb)
+        {
+            //proceed to check whether the origin is within the AB, AC or ABC voronoi region
+            goto face_check1;
+        }
+        else if (!over_abc && over_acd && !over_adb)
+        {
+            //rotate ACD into ABC
+
+            //point c into b & point d into c
+            simplex[2] = simplex[1];
+            simplex[1] = simplex[0];
+
+            //rest is self-explanatory
+            ab = ac;
+            ac = ad;
+
+            normal_abc = normal_acd;
+
+            //after rotating we do the exact same as above
+            goto face_check1;
+        }
+        else if (!over_abc && !over_acd && over_adb)
+        {
+            //rotate ADB into ABC
+
+            //point b into c & point d into c
+            simplex[1] = simplex[2];
+            simplex[2] = simplex[0];
+
+            //rest is self-explanatory
+            ac = ab;
+            ab = ad;
+
+            normal_abc = normal_adb;
+
+            //after rotating we do the exact same as above
+            goto face_check1;
+        }
+        else if (over_abc && over_acd && !over_adb)
+        {
+            goto double_face_check;
+        }
+        else if (!over_abc && over_acd && over_adb)
+        {
+            //Rotate ACD into ABC and ADB into ACD
+            //b goes into tmp
+            glm::vec3 tmp = simplex[2];
+            //c goes into b
+            simplex[2] = simplex[1];
+            //d goes into c
+            simplex[1] = simplex[0];
+            //b goes into d
+            simplex[0] = tmp;
+
+            //rest is self-explanatory
+            tmp = ab;
+            ab = ac;
+            ac = ad;
+            ad = tmp;
+
+            normal_abc = normal_acd;
+            normal_acd = normal_adb;
+
+            goto double_face_check;
+        }
+        else if (over_abc && !over_acd && over_adb)
+        {
+            //Rotate ABD into ABC and ABC into ACD
+            //c goes into tmp
+            glm::vec3 tmp = simplex[1];
+            //b into c
+            simplex[1] = simplex[2];
+            //d into b
+            simplex[2] = simplex[0];
+            //c into d
+            simplex[0] = tmp;
+
+            //rest is self-explanatory
+            tmp = ac;
+            ac = ab;
+            ab = ad;
+            ad = tmp;
+
+            normal_acd = normal_abc;
+            normal_abc = normal_adb;
+
+            goto double_face_check;
+        }
+        else
+        {
+            //This is the edgiest of edge cases:
+            //You have somehow constructed a tetrahedron of 4 points on a plane that DOES CONTAIN the origin.
+            //That is technically a case where the 2 objects do touch but do not overlap.
+            //That, or your point a lies EXACTLY at 0, 0, 0,
+            //or you have constructed some other degenerate 4 vertex shape that's impossible for me to even wrap my head around.
+            //Whichever the case - I congratulate you!
+            //Have a collision as a treat - I'm returning true
+            return true;
+        }
+
+        face_check1:
+        //check if origin is in the AC Voronoi region
+        if (glm::dot(glm::cross(normal_abc, ac), ao) > 0)
+        {
+            //this case means we must remove 2 points and go back to a triangle case
+            //set the direction the same as in a triangle case
+            direction = tripleCrossProduct(ac, ao);
+
+            //remove b & d
+            simplex.erase(simplex.begin() + 2);
+            simplex.erase(simplex.begin());
+
+            //no collision - return false
+            return false;
+        }
+
+        face_check2:
+        //check if origin is in the AB Voronoi region
+        if (glm::dot(glm::cross(normal_abc, ac), ao) > 0)
+        {
+            //this case means we must remove 2 points and go back to a triangle case
+            //set the direction the same as in a triangle case
+            direction = tripleCrossProduct(ab, ao);
+
+            //remove c & d
+            simplex.erase(simplex.begin() + 1);
+            simplex.erase(simplex.begin());
+
+            //no collision - return false
+            return false;
+        }
+
+        //if not in either AC nor AB it must be in ABC Voronoi region
+        //we set ABC as the new triangle and continue as before
+        direction = normal_abc;
+        simplex.erase(simplex.begin());
+        //still no collision
+        return false;
+
+        double_face_check:
+
+        if (glm::dot(glm::cross(normal_abc, ac), ao) > 0)
+        {
+            //looking from ABC's perspective the origin is beyond AC
+            //this means we only need to rerun the single face test for ACD
+
+            //rotate ACD into ABC
+            //rotate ACD into ABC
+
+            //point c into b & point d into c
+            simplex[2] = simplex[1];
+            simplex[1] = simplex[0];
+
+            //rest is self-explanatory
+            ab = ac;
+            ac = ad;
+
+            normal_abc = normal_acd;
+
+            goto face_check1;
+        }
+
+        //if we got here we know we can only be in the AB or ABC Voronoi region
+        //as such all that is left to do is to rerun the 2nd half of 1 face test
+        goto face_check2;
+
         return false;
     }
 
@@ -194,6 +382,7 @@ public:
 
     std::vector<PhysicsElement> getElements()
     {
+        //todo
         return {};
     }
 };
