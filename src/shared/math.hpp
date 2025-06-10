2fa60395cc7e362e73f86c908f80bfdd86505be6
skay44 <michal.1805@wp.pl>
2025-06-11 00:40:42 +0200

    added comments to math.hpp

--- src/shared/math.hpp
+++ src/shared/math.hpp
@@ -4,14 +4,31 @@
 #include "ease.hpp"
 
 namespace math {
-
 	extern const glm::vec3 FORWARD;
 	extern const glm::vec3 UP;
 
+	/**
+	 * rotates given vector by quaterion
+	 * @param q quaternion used to rotate the vector
+	 * @param vec vector that's rotated by quaterion
+	 * @return rotated vector
+	 */
 	glm::vec3 quaternionRotation(glm::quat q, glm::vec3 vec);
 
+	/**
+	 * calculates facing direction of an object
+	 * rotated by quaternion rotation parameter in 3D space
+	 * @param rotation rotation of an object
+	 * @return facting direction of an object
+	 */
 	glm::vec3 calculateForwardVector(glm::quat rotation);
 
+	/**
+	 * calculate facing direction of an object
+	 * rotated by given angle in 2D space
+	 * @param angle
+	 * @return
+	 */
 	glm::vec2 calculateForwardVector2D(float angle);
 
 	/**
@@ -23,6 +40,11 @@ namespace math {
 	 */
 	glm::vec3 rotateAlongAxis(glm::vec3 normal, float angle);
 
+	/**
+	 * transforms translation vector into translation matrix
+	 * @param vector vector to be converted into matrix
+	 * @return matrix resulting from the convertion
+	 */
 	glm::mat4 translationVectorToMatrix(glm::vec3 vector);
 
 	/**
@@ -51,10 +73,9 @@ namespace math {
 	 */
 	size_t alignUp(size_t size, size_t alignment);
 
-    /**
-     * Calculate the value vector of a x b x a
-     * @return vector value of a x b x a
-     */
-    glm::vec3 tripleCrossProduct(glm::vec3& a, glm::vec3& b);
-
+	/**
+	 * Calculate the value vector of a x b x a
+	 * @return vector value of a x b x a
+	 */
+	glm::vec3 tripleCrossProduct(glm::vec3& a, glm::vec3& b);
 }
