a21c0828b1407807b701bf4e46357d6e06548cd2
magistermaks <magistermaks@darktree.net>
2025-09-29 21:10:20 +0200

    Fixed #25

--- src/main.cpp
+++ src/main.cpp
@@ -42,12 +42,12 @@ static void entry(Args& args) {
 	auto context = RenderSystemOverlay::create(theme);
 
 	//window.getInputDispatcher().registerListener(std::make_shared<DebugInputListener>());
-	window.getInputDispatcher().registerListener(context);
+	window.getInputDispatcher().registerListener(context, 1);
 	auto models = system.importObj("assets/models/checklight.obj");
 	auto cube = system.importObj("assets/models/cube.obj");
 
 	auto dispacher = std::make_shared<InputDispatcher>();
-	window.getInputDispatcher().registerListener(dispacher);
+	window.getInputDispatcher().registerListener(dispacher, 2);
 	BoardManager manager(dispacher);
 	manager.setGravity(glm::vec3(0, -10, 0));
 
